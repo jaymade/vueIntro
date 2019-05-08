8440df33d572ecfe9d9931bfb6fcc9785d14b7ab
jayrich <jay@jaymade.com>
2019-05-08 15:43:30 -0400

    added color blocks

--- main.js
+++ main.js
@@ -8,18 +8,26 @@ var app = new Vue({
         details: ["80% cotton", "20% poly", "100% awesome"],
         variants: [{
                 vid: 2223,
-                vColor: 'Green'
+                vColor: 'green',
+                vImage: './assets/vmSocks-green-onWhite.jpg'
             },
             {
                 vid: 2224,
-                vColor: 'Blue'
+                vColor: 'blue',
+                vImage: './assets/vmSocks-blue-onWhite.jpg'
             }
         ],
         cart: 0,
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1;
         },
-         methods: {
-                 addToCart: function () {
-                     this.cart += 1;
-                 }
-                }
-    });
+        removeFromCart: function () {
+               this.cart -= 1;
+        },
+        updateProduct: function (vImage) {
+            this.image = vImage;
+        }
+    }
+});
